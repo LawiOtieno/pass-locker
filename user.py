89bd594a66180f2635f3e39449e004faa83144df
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-09-06 18:48:33 +0300

    user account method, find credentials

--- user.py
+++ user.py
@@ -1,90 +1,90 @@
-# import unittest
-# from credentials import Credentials  # Import credentials class
+import unittest
+from credentials import Credentials  # Import credentials class
 
 
-# class User:
+class User:
 
-#     '''
-#     Class that generates new instances of user account
-#     '''
+    '''
+    Class that generates new instances of user account
+    '''
 
-#     user_list = []  # Empty user list
+    user_list = []  # Empty user list
 
-#     def __init__(self, user_name, user_password):
+    def __init__(self, user_name, user_password):
 
         
-#         '''
-#         __init__ method to define the properties of a User object
-#         Args:
-#             user_name: Name of the user 
-#             user_password: Password of the user
-#         '''
+        '''
+        __init__ method to define the properties of a User object
+        Args:
+            user_name: Name of the user 
+            user_password: Password of the user
+        '''
 
-#         self.user_name = user_name
-#         self.user_password = user_password
+        self.user_name = user_name
+        self.user_password = user_password
 
-#     def save_user(self):
+    def save_user(self):
 
-#         '''
-#         Save user in user_list 
-#         '''
+        '''
+        Save user in user_list 
+        '''
 
-#         User.user_list.append(self)
+        User.user_list.append(self)
 
     
     
-#     # Find credentials of user
-#     @classmethod
-#     def find_credentials(cls, name):
+    # Find credentials of user
+    @classmethod
+    def find_credentials(cls, name):
         
-#         '''
-#         Checking for correct importation
-#         Args:
-#             name: Credential name
-#         Returns:
-#             Boolean: True or False
-#         '''
+        '''
+        Checking for correct importation
+        Args:
+            name: Credential name
+        Returns:
+            Boolean: True or False
+        '''
 
-#         # Search in the user list
-#         for credentials in Credentials.credentials_list:
-#             if credentials.credentials_name == name:
-#                 return True
+        # Search in the user list
+        for credentials in Credentials.credentials_list:
+            if credentials.credentials_name == name:
+                return True
 
-#         return False
-
-    
-    
-#     # For User to login
-#     @classmethod
-#     def log_in(cls, name, password):
-
-#         '''
-#         Method for user to log into his/her account
-#         Args:
-#             name: name of user
-#             password: Associated user password
-#         Returns: 
-#             User credentials: If name & password match.
-#             False: If the name & password don't match
-#         '''
-
-#         # Search and compare user list
-#         for user in cls.user_list:
-#             if user.user_name == name and user.user_password == password:
-#                 return Credentials.credentials_list
-
-#         return False
+        return False
 
     
-#     # Returning user_list
-#     @classmethod
-#     def display_user(cls):
+    
+    # For User to login
+    @classmethod
+    def log_in(cls, name, password):
+
+        '''
+        Method for user to log into his/her account
+        Args:
+            name: name of user
+            password: Associated user password
+        Returns: 
+            User credentials: If name & password match.
+            False: If the name & password don't match
+        '''
+
+        # Search and compare user list
+        for user in cls.user_list:
+            if user.user_name == name and user.user_password == password:
+                return Credentials.credentials_list
+
+        return False
+
+    
+    # Returning user_list
+    @classmethod
+    def display_user(cls):
         
-#         '''
-#         Method that returns the user list
-#         '''
+        '''
+        Method that returns the user list
+        '''
 
-#         return cls.user_list
+        return cls.user_list
 
 #     @classmethod
 #     def user_exists(cls, name):
